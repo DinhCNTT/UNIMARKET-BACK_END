03d4abe86db1a8b95f83ae8249794f66e8e6ec86
caolam23 <caol69265@gmail.com>
2025-08-12 19:15:21 +0700

    feat: thêm chức năng lưu video

---
+++ UniMarket/Migrations/20250809153021_FixVideoTinDangSaveCascade.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace UniMarket.Migrations
+{
+    /// <inheritdoc />
+    public partial class FixVideoTinDangSaveCascade : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "VideoTinDangSaves",
+                columns: table => new
+                {
+                    MaVideoSave = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    MaTinDang = table.Column<int>(type: "int", nullable: false),
+                    MaNguoiDung = table.Column<string>(type: "nvarchar(450)", nullable: false),
+                    NgayLuu = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_VideoTinDangSaves", x => x.MaVideoSave);
+                    table.ForeignKey(
+                        name: "FK_VideoTinDangSaves_AspNetUsers_MaNguoiDung",
+                        column: x => x.MaNguoiDung,
+                        principalTable: "AspNetUsers",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                    table.ForeignKey(
+                        name: "FK_VideoTinDangSaves_TinDangs_MaTinDang",
+                        column: x => x.MaTinDang,
+                        principalTable: "TinDangs",
+                        principalColumn: "MaTinDang",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_VideoTinDangSaves_MaNguoiDung",
+                table: "VideoTinDangSaves",
+                column: "MaNguoiDung");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_VideoTinDangSaves_MaTinDang",
+                table: "VideoTinDangSaves",
+                column: "MaTinDang");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "VideoTinDangSaves");
+        }
+    }
+}
--- UniMarket/obj/Debug/net9.0/UniMarket.AssemblyInfo.cs
+++ UniMarket/obj/Debug/net9.0/UniMarket.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("UniMarket")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+58dc1058b125ba2e2c661e93a61eb1928194bc53")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2b89314bc96cb2c7757f74869a5e9c68ace8dd21")]
 [assembly: System.Reflection.AssemblyProductAttribute("UniMarket")]
 [assembly: System.Reflection.AssemblyTitleAttribute("UniMarket")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
